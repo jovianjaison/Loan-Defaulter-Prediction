9815b9f35b43f7541dadc8b934b63562e04a6cf9
jovianjaison <jovianjaison@gmail.com>
2018-08-17 12:19:25 +0530

    Normalization of dataset

--- proj_bank_fin.py
+++ proj_bank_fin.py
@@ -1,16 +1,24 @@
 import csv
 import random
-def localDataset(filename,Set=[]):
+def localTrainDataset(filename,Set=[]):
 	with open(filename) as csvfile:
 		lines = csv.reader(csvfile)
 		dataset = list(lines)
-		for x in range(len(dataset)-1):
-			for y in range(4):
-				dataset[x][y] = float(dataset[x][y])
-				Set.append(dataset[x])
+		for x in range(1, len(dataset)):
+			for y in range(16):
+				dataset[x][y] = dataset[x][y]
+			Set.append(dataset[x])
+def localTestDataset(filename,Set=[]):
+	with open(filename) as csvfile:
+		lines = csv.reader(csvfile)
+		dataset = list(lines)
+		for x in range(1, len(dataset)):
+			for y in range(15):
+				dataset[x][y] = dataset[x][y]
+			Set.append(dataset[x])
 trainingSet=[]
 testSet=[]
-localDataset('credit_train.csv',trainingSet)
-localDataset('credit_test.csv',testSet)
+localTrainDataset('credit_train.csv',trainingSet)
+localTestDataset('credit_test.csv',testSet)
 print('Train:'+repr(len(trainingSet)))
 print('Test:'+repr(len(testSet)))
